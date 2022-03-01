03c71237b1da42af964700913b673433cd5aa179
mkxgl <marta.gludkowska@gmail.com>
2022-03-01 15:10:52 +0100

    changed name

--- simonsays/mipslab.h
+++ simonsays/mipslab.h
@@ -1,23 +1,37 @@
 /* mipslab.h
+<<<<<<< Updated upstream:simonsays/mipslab.h
    Header file for all labs.
    This file written 2015 by F Lundevall
+=======
+   Header file for the game Simon Says.
+   This file written 2022 by MK Gludkowska
+>>>>>>> Stashed changes:simonsays/simon.h
    Some parts are original code written by Axel Isaksson
 
+<<<<<<< Updated upstream:simonsays/mipslab.h
    Latest update 2015-08-28 by F Lundevall
 
    For copyright and licensing, see file COPYING */
 
 /* Declare display-related functions from mipslabfunc.c */
+=======
+/* Declare display-related functions from mipslabwork.c */
+>>>>>>> Stashed changes:simonsays/simon.h
 void display_image(int x, const uint8_t *data);
 void display_init(void);
 void display_string(int line, char *s);
 void display_update(void);
 uint8_t spi_send_recv(uint8_t data);
 
+<<<<<<< Updated upstream:simonsays/mipslab.h
 /* Declare lab-related functions from mipslabfunc.c */
 char * itoaconv( int num );
 void labwork(void);
 int nextprime( int inval );
+=======
+/* Declare game-related functions from mipslabwork.c */
+char * itoaconv( int num );;
+>>>>>>> Stashed changes:simonsays/simon.h
 void quicksleep(int cyc);
 void tick( unsigned int * timep );
 
